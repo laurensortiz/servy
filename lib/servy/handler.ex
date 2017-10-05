f6f298ffe034793526f246fa5a81542191658f7d
Alejandro Nanez <alejonanez@gmail.com>
2017-10-04 19:39:30 -0500

    Request params + Status code - Trying to delete a bear - Created a fallback route

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -13,7 +13,12 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split(" ")
 
-    %{ method: method, path: path, resp_body: "" }
+    %{
+      method: method,
+      path: path,
+      resp_body: "",
+      status: nil
+    }
   end
 
   def route(conv) do
@@ -21,32 +26,63 @@ defmodule Servy.Handler do
   end
 
   def route(conv, "GET", "/wildthings") do
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+    %{ conv | resp_body: "Bears, Lions, Tigers", status: 200 }
   end
 
   def route(conv, "GET", "/bears") do
-    %{ conv | resp_body: "Teddy, Paddington, Smokey" }
+    %{ conv | resp_body: "Teddy, Paddington, Smokey", status: 200 }
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{ conv | resp_body: "Bear #{id}", status: 200 }
+  end
+
+  def route(conv, "DELETE", "/bears/" <> _id) do
+    %{ conv | resp_body: "You can't delete!", status: 403 }
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | resp_body: "No #{path} here", status: 404 }
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
   end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
 end
 
 request = """
-GET /wildthings HTTP/1.1
+GET /bears/4 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
+"""
 
+request_delete = """
+DELETE /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
 """
 
 response = Servy.Handler.handle(request);
+response_delete = Servy.Handler.handle(request_delete);
 
 IO.puts response
+IO.puts response_delete
