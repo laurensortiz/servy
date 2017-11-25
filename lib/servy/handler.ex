fef599399507f624e7751f2810816569e295d46a
Alejandro Nanez <alejonanez@gmail.com>
2017-11-25 18:40:42 -0500

    Add structs for type checking

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -4,7 +4,12 @@ defmodule Servy.Handler do
     Handles HTTP requests.
   """
 
+  alias Servy.Conv
+
   @pages_path Path.expand("../../pages", __DIR__)
+
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+  import Servy.Parser, only: [parse: 1]
 
   @doc """
     Transformst the request into a response.
@@ -19,53 +24,19 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def track(%{ status: 404, path: path } = conv) do
-    IO.puts "Oops this route doesn't exist! #{path}"
-    conv
-  end
-
-  def track(conv), do: conv
-
-  def rewrite_path(%{ path: "/wildlife" } = conv) do
-    %{ conv | path: "/wildthings" }
-  end
-
-  def rewrite_path(%{ path: "/bears?id=" <> id } = conv) do
-    %{ conv | path: "/bears/" <> id }
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def log(conv), do: IO.inspect conv
-
-  def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first
-      |> String.split(" ")
-
-    %{
-      method: method,
-      path: path,
-      resp_body: "",
-      status: nil
-    }
-  end
-
-  def route(%{ method: "GET", path: "/wildthings" } = conv) do
+  def route(%Conv{ method: "GET", path: "/wildthings" } = conv) do
     %{ conv | resp_body: "Bears, Lions, Tigers", status: 200 }
   end
 
-  def route(%{ method: "GET", path: "/bears" } = conv) do
+  def route(%Conv{ method: "GET", path: "/bears" } = conv) do
     %{ conv | resp_body: "Teddy, Paddington, Smokey", status: 200 }
   end
 
-  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
+  def route(%Conv{ method: "GET", path: "/bears/" <> id } = conv) do
     %{ conv | resp_body: "Bear #{id}", status: 200 }
   end
 
-  def route(%{ method: "GET", path: "/about/" } = conv) do
+  def route(%Conv{ method: "GET", path: "/about/" } = conv) do
     @pages_path
       |> Path.join("about.html")
       |> File.read
@@ -84,33 +55,22 @@ defmodule Servy.Handler do
     %{ conv | status: 500, resp_body: "File error #{reason}" }
   end
 
-  def route(%{ method: "DELETE", path: "/bears/" <> id } = conv) do
+  def route(%Conv{ method: "DELETE", path: "/bears/" <> id } = conv) do
     %{ conv | resp_body: "You can't delete bear # #{id}!", status: 403 }
   end
 
-  def route(%{ path: path } = conv) do
+  def route(%Conv{ path: path } = conv) do
     %{ conv | resp_body: "No #{path} here", status: 404 }
   end
 
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
-  end
-
-  defp status_reason(code) do
-    %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Internal Server Error"
-    }[code]
   end
 end
 
